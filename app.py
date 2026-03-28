af7ad0092f29c806cf3d8d981e470aabb7490ef5
chinnimunni101217-hash <chinnimunni101217@gmail.com>
2026-03-28 19:49:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import streamlit as st
+from deep_translator import GoogleTranslator
+from gtts import gTTS
 
+st.title("🌍 AI Translator App")
+
+text = st.text_area("Enter text:")
+
+lang = st.selectbox("Select Language", ["hi","ta","te","kn","ml","en"])
+
+if st.button("Translate"):
+    if text:
+        result = GoogleTranslator(source="auto", target=lang).translate(text)
+        st.success(result)
+
+        tts = gTTS(result, lang=lang)
+        tts.save("voice.mp3")
+        st.audio("voice.mp3")
